9ac0145807a942cca0ce77fa6b30f3c836596b96
Martin Braunsperger <martin.braunsperger@gmail.com>
2023-04-08 01:27:48 +0200

    Remove develop example

--- examples/develop.rs
+++
@@ -1,8 +0,0 @@
-use processes::{self, Process};
-
-pub fn main() {
-    let process = processes::find_first_by_name("firefox").unwrap().unwrap();
-    for module in process.modules().unwrap() {
-        println!("\t{}", module.base_name().unwrap());
-    }
-}
